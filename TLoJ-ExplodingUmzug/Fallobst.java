060d54cfb5a0ada8b0ef0a362d15de4e623438ab
SoulthiefofDead <38541339+SoulthiefofDead@users.noreply.github.com>
2018-04-21 01:33:06 +0200

    .

--- TLoJ-ExplodingUmzug/Fallobst.java
+++ TLoJ-ExplodingUmzug/Fallobst.java
@@ -4,5 +4,6 @@ public abstract class Fallobst
     protected GLObjekt objekt1,objekt2;
     public void fallen(){
         objekt1.verschiebe(0,-1,0);
+        objekt2.verschiebe(0,-1,0);
     }
 }
--- TLoJ-ExplodingUmzug/main.java
+++ TLoJ-ExplodingUmzug/main.java
@@ -4,10 +4,21 @@ public class main
     private GLSchwenkkamera kamera;
     private GLLicht licht;
     private Apfel apfel;
+    private Jannes jannes;
     public main()
     {
         kamera = new GLSchwenkkamera();
         licht = new GLLicht();
         apfel = new Apfel(0,0,0,10);
+        
+        fuehreAus();
+    }
+    
+    public void fuehreAus(){
+        Sys.warte(1000);
+        for(int i = 0; i < 50; i++){
+            apfel.fallen();
+            Sys.warte(100);
+        }
     }
 }
