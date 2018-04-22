0199fb560edc480557c6108e0c0d20c37f4c2955
GreenLonk <maxi7845.f@gmail.com>
2018-04-22 22:56:03 +0200

    Nebel mit blauer Farbe hinzugefügt

--- main.java
+++ main.java
@@ -5,6 +5,7 @@ public class main
     private GLLicht licht;
     private GLBoden gras;
     private GLHimmel himmel;
+    private GLNebel nebel;
     
     private Apfel apfel;
     //private Waschmaschine waschmaschine;
@@ -18,6 +19,7 @@ public class main
         licht = new GLLicht();
         gras = new GLBoden("Texturen/Grastextur.jpg");
         himmel = new GLHimmel("Texturen/Himmel.png");
+        nebel = new GLNebel();
         
         apfel = new Apfel(0,20,0,20);
         //waschmaschine = new Waschmaschine(0,0,0,100);
@@ -25,7 +27,11 @@ public class main
         boden = new Spielfläche(500);
         
         jannes = new Jannes(100,0,0,40);
+        
+        nebel.setzeNebelbereich(1000,4000);
+        nebel.setzeFarbe(0.4,0.7,1);
         //fuehreAus();
+        
     }
     
     public void fuehreAus(){
