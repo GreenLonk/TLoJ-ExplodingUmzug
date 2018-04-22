cbbcd26126115b004bab84e0f876ec3e418e5142
GreenLonk <maxi7845.f@gmail.com>
2018-04-22 11:19:35 +0200

    Boden
    Neue Methode Grundfläche(pLaenge), mit der eine Grundfläche für den Player inkl Zäunen erstellt wird (wip)
    
    Grastextur als Boden, Schwenkkamera mit Entwicklerkamera ausgetauscht

--- main.java
+++ main.java
@@ -1,18 +1,21 @@
 import GLOOP.*;
 public class main
 {
-    private GLSchwenkkamera kamera;
+    private GLEntwicklerkamera kamera;
     private GLLicht licht;
     private Apfel apfel;
     private Waschmaschine waschmaschine;
     private Jannes jannes;
+    private Grundfläche boden;
+    private GLBoden gras;
     public main()
     {
-        kamera = new GLSchwenkkamera();
+        kamera = new GLEntwicklerkamera();
         licht = new GLLicht();
+        gras = new GLBoden("Grastextur.jpg");
         apfel = new Apfel(0,0,0,10);
         waschmaschine = new Waschmaschine(0,0,0,100);
-        
+        boden = new Grundfläche(1000);
         //fuehreAus();
     }
     
