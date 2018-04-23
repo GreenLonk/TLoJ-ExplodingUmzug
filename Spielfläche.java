e24e75ef567826dfb533b647c7c406644b3c398d
GreenLonk <maxi7845.f@gmail.com>
2018-04-23 22:54:14 +0200

    Zäune geaddtet

--- Spielfläche.java
+++ Spielfläche.java
@@ -10,8 +10,15 @@ public class Spielfläche
     public Spielfläche(int pLaenge)
     {
         boden = new GLQuader(0,0,0,pLaenge,1,pLaenge);
-        zaunl = new GLQuader((pLaenge/2),(pLaenge/20),0,(pLaenge/100),(pLaenge/10),pLaenge);
+        zaunl = new GLQuader(-(pLaenge/2),(pLaenge/20),0,(pLaenge/100),(pLaenge/10),pLaenge);
+        zaunr = new GLQuader((pLaenge/2),(pLaenge/20),0,(pLaenge/100),(pLaenge/10),pLaenge);
+        zaunv = new GLQuader(0,(pLaenge/20),(pLaenge/2),pLaenge,(pLaenge/10),(pLaenge/100));
+        zaunh = new GLQuader(0,(pLaenge/20),-(pLaenge/2),pLaenge,(pLaenge/10),(pLaenge/100));
+        
         boden.setzeTextur("Texturen/Spielfläche.jpg");
         zaunl.setzeTextur("Texturen/Zaun.jpg");
+        zaunr.setzeTextur("Texturen/Zaun.jpg");
+        zaunv.setzeTextur("Texturen/Zaun.jpg");
+        zaunh.setzeTextur("Texturen/Zaun.jpg");
     }
 }
